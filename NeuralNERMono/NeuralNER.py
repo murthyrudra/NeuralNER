b1eb4cc6baf639cfac39e54fc641d711f84a4a10
Rudra Murthy <rudramurthy.iisc@gmail.com>
2018-09-04 10:16:49 +0530

    Updated NeuralNER.py
    Added code for performing testing and deploying the model

--- NeuralNERMono/NeuralNER.py
+++ NeuralNERMono/NeuralNER.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
-
+# Parts of the code are taken from NeuroNLP2 https://github.com/XuezheMax/NeuroNLP2/
 __author__ = 'rudramurthy'
 """
-Implementation of Neural NER.
+Implementation of Bi-directional LSTM-CNNs model for NER.
 """
 
 import os
@@ -14,20 +14,19 @@ sys.path.append("..")
 
 import time
 import argparse
-import uuid
+import fnmatch
 
 import numpy as np
 import torch
 from torch.optim import Adam, SGD
-from models.modules import BiCNNLSTMTranstion
+from models.modules import BiCNNLSTMTranstion, BiCNNAverageTranstion
 from utils.utilsLocal import *
+from utils.CoNLLData import CoNLL
 from torch.utils.data import DataLoader
+from torch.nn.parameter import Parameter
 
-uid = uuid.uuid4().hex[:6]
-
-# The following function is copied from the Neuro NLP code made available at https://github.com/XuezheMax/NeuroNLP
 def evaluate(output_file, save_dir):
-	score_file = "%s/score_%s" % (save_dir, str(uid))
+	score_file = "%s/score_" % (save_dir)
 	os.system("eval/conll03eval.v2 < %s > %s" % (output_file, score_file))
 	with open(score_file, 'r') as fin:
 		fin.readline()
@@ -41,28 +40,34 @@ def evaluate(output_file, save_dir):
 
 
 def main():
-	parser = argparse.ArgumentParser(description='Tuning with bi-directional LSTM-CNN')
+	parser = argparse.ArgumentParser(description='Training a Sequence Labeler with bi-directional LSTM-CNN')
 	parser.add_argument('--num_epochs', type=int, default=100, help='Number of training epochs')
 	parser.add_argument('--batch_size', type=int, default=5, help='Number of sentences in each batch')
 	parser.add_argument('--hidden_size', type=int, default=200, help='Number of hidden units in RNN')
-	parser.add_argument('--num_filters', type=int, default=15, help='Number of filters in CNN')
-	parser.add_argument('--min_filter_width', type=int, default=1, help='Number of filters in CNN')
+	parser.add_argument('--num_filters', type=int, default=35, help='Number of filters in CNN')
+	parser.add_argument('--min_filter_width', type=int, default=3, help='Number of filters in CNN')
 	parser.add_argument('--max_filter_width', type=int, default=7, help='Number of filters in CNN')
 
 	parser.add_argument('--learning_rate', type=float, default=0.4, help='Learning rate')
-	parser.add_argument('--decay_rate', type=float, default=0.1, help='Decay rate of learning rate')
 	parser.add_argument('--gamma', type=float, default=0.0, help='weight for regularization')
-	parser.add_argument('--schedule', type=int, default=1, help='schedule for learning rate decay')
 	parser.add_argument('--embedding_dict', help='path for embedding dict')
+	parser.add_argument('--embedding_dict_new', help='path for embedding dict')
 	parser.add_argument('--train')
 	parser.add_argument('--dev')
 	parser.add_argument('--test')
+	parser.add_argument('--vocabChar')
+	parser.add_argument('--vocabTag')
 
 	parser.add_argument('--ner_tag_field', type=int, default=1, help='ner tag field')
 	parser.add_argument('--use_gpu', type=int, default=1, help='use gpu')
 
+	parser.add_argument('--fineTune', type=bool, default=False, help='fineTune pretrained word embeddings')
+
 	parser.add_argument('--save-dir')
 	parser.add_argument('--perform_evaluation', type=bool, default=False, help='perform evaluation only')
+	parser.add_argument('--deploy', type=bool, default=False, help='deploy')
+
+	parser.add_argument('--train_from', type=str, default="")
 
 	args = parser.parse_args()
 
@@ -80,9 +85,7 @@ def main():
 
 	learning_rate = args.learning_rate
 	momentum = 0.01 * learning_rate
-	decay_rate = args.decay_rate
 	gamma = args.gamma
-	schedule = args.schedule
 
 	embedding_path = args.embedding_dict
 
@@ -100,29 +103,59 @@ def main():
 	charVocabulary = dict()
 	charReverseVocabulary = []
 
-	charVocabulary["<S>"] = len(charVocabulary)
-	charReverseVocabulary.append("<S>")
-	charVocabulary["</S>"] = len(charVocabulary)
-	charReverseVocabulary.append("</S>")
-
 	tagVocabulary = dict()
 	tagReverseVocabulary = []
 
-	trainCorpus, trainLabelsRaw, maxTrainLength = readCoNLL(train_path, charVocabulary, tagVocabulary, charReverseVocabulary, tagReverseVocabulary, args.ner_tag_field)
-	print("Train Corpus contains " + str(len(trainCorpus)) + " sentences and maximum sentence length is " + str(maxTrainLength))
+	if args.vocabChar:
+		loadVocabulary(args.vocabChar, charVocabulary, charReverseVocabulary)
+		print("Read " + str(len(charVocabulary)) + " number of characters")
 
-	trainCorpusRawSorted, trainLabelsRawSorted = sortTrainData(trainCorpus, trainLabelsRaw)
-	print("Sorted the train Corpus based on length ")
+	if args.vocabTag:
+		loadVocabulary(args.vocabTag, tagVocabulary, tagReverseVocabulary)
+		print(tagVocabulary)
 
-	devCorpus, devLabelsRaw, maxDevLength = readCoNLL(dev_path, charVocabulary, tagVocabulary, charReverseVocabulary, tagReverseVocabulary, args.ner_tag_field)
-	testCorpus, testLabelsRaw, maxTestLength = readCoNLL(test_path, charVocabulary, tagVocabulary, charReverseVocabulary, tagReverseVocabulary, args.ner_tag_field)
+	if evaluation:
+		if not args.deploy:
+			testCorpus, testLabelsRaw, maxTestLength = readCoNLL(test_path, charVocabulary, tagVocabulary, charReverseVocabulary, tagReverseVocabulary, args.ner_tag_field, embedding_vocab, args.vocabTag, args.vocabChar)
+			print("Test Corpus contains " + str(len(testCorpus)) + " sentences and maximum sentence length is " + str(maxTestLength))
+			print("Read " + str(len(charVocabulary)) + " number of characters")
 
-	print("Dev Corpus contains " + str(len(devCorpus)) + " sentences and maximum sentence length is " + str(maxDevLength))
-	print("Test Corpus contains " + str(len(testCorpus)) + " sentences and maximum sentence length is " + str(maxTestLength))
+		else:
+			testCorpus, maxTestLength = readUnlabeledData(test_path)
+			print("Test Corpus contains " + str(len(testCorpus)) + " sentences and maximum sentence length is " + str(maxTestLength))
+	else:
+		if not args.vocabChar:
+			charVocabulary["<S>"] = len(charVocabulary)
+			charReverseVocabulary.append("<S>")
 
-	print("Read " + str(len(charVocabulary)) + " number of characters")
+			charVocabulary["</S>"] = len(charVocabulary)
+			charReverseVocabulary.append("</S>")
 
-	print(tagVocabulary)
+		trainCorpus, trainLabelsRaw, maxTrainLength = readCoNLL(train_path, charVocabulary, tagVocabulary, charReverseVocabulary, tagReverseVocabulary, args.ner_tag_field, embedding_vocab, args.vocabTag, args.vocabChar)
+		print("Train Corpus contains " + str(len(trainCorpus)) + " sentences and maximum sentence length is " + str(maxTrainLength))
+
+		# trainCorpusRawSorted, trainLabelsRawSorted = sortTrainData(trainCorpus, trainLabelsRaw)
+		trainCorpusRawSorted = trainCorpus
+		trainLabelsRawSorted = trainLabelsRaw
+		print("Sorted the train Corpus based on length ")
+
+		devCorpus, devLabelsRaw, maxDevLength = readCoNLL(dev_path, charVocabulary, tagVocabulary, charReverseVocabulary, tagReverseVocabulary, args.ner_tag_field, embedding_vocab, args.vocabTag, args.vocabChar)
+		print("Dev Corpus contains " + str(len(devCorpus)) + " sentences and maximum sentence length is " + str(maxDevLength))
+
+		testCorpus, testLabelsRaw, maxTestLength = readCoNLL(test_path, charVocabulary, tagVocabulary, charReverseVocabulary, tagReverseVocabulary, args.ner_tag_field, embedding_vocab, args.vocabTag, args.vocabChar)
+		print("Test Corpus contains " + str(len(testCorpus)) + " sentences and maximum sentence length is " + str(maxTestLength))
+
+		if not args.vocabChar:
+			print("Read " + str(len(charVocabulary)) + " number of characters")
+
+			print(tagVocabulary)
+
+		if not args.vocabChar:
+			tmp_filename = '%s/vocab' % (save_dir)
+			saveVocabulary(tmp_filename, charVocabulary, charReverseVocabulary)
+
+			tmp_filename = '%s/tag' % (save_dir)
+			saveVocabulary(tmp_filename, tagVocabulary, tagReverseVocabulary)
 
 	print("Number of epochs = " +  str(num_epochs))
 	print("Mini-Batch size = " +  str(batch_size))
@@ -132,17 +165,18 @@ def main():
 	print("Maximum ngrams for CNN filter = " +  str(max_filter_width))
 	print("Initial Learning Rate = " +  str(learning_rate))
 
-	use_gpu = torch.cuda.is_available()
+	use_gpu = args.use_gpu
 
-	network = BiCNNLSTMTranstion(vocabularySize, embeddingDimension, min_filter_width, max_filter_width, len(charVocabulary), num_filters, hidden_size, len(tagVocabulary) , embedd_dict)
+	network = BiCNNLSTMTranstion(vocabularySize, embeddingDimension, min_filter_width, max_filter_width, len(charVocabulary), num_filters, hidden_size, len(tagVocabulary) , embedd_dict, args.fineTune)
 
-	# network = BiLSTM(vocabularySize, embeddingDimension, hidden_size, len(tagVocabulary) , embedd_dict)
+	print(network)
 
 	lr = learning_rate
 
 	optim = SGD(network.parameters(), lr=lr, momentum=momentum, weight_decay=gamma, nesterov=True)
 
-	num_batches = len(trainCorpus) / batch_size + 1
+	if not evaluation:
+		num_batches = len(trainCorpus) / batch_size + 1
 
 	dev_f1 = 0.0
 	dev_acc = 0.0
@@ -154,41 +188,99 @@ def main():
 	test_recall = 0.0
 	best_epoch = 0
 
-	print("Training....")
-
-	if args.use_gpu == 1:
-		network.cuda()
-
 	if evaluation:
 		network.load_state_dict(torch.load(save_dir + "/model"))
 
+		print("Performing Evaluation")
+		if args.use_gpu == 0:
+			network.cpu()
+
+# This part of the code is buggy and needs debugging
+		if args.embedding_dict_new:
+			network.cpu()
+			print("Loading new embeddings")
+
+			load_embeddings_new(args.embedding_dict_new, embedding_vocab, reverse_word_vocab, embedd_dict)
+			print("Read Word Embedding for " + str(len(embedding_vocab)) + " number of words")
+
+			vocabularySize_new = len(embedding_vocab)
+
+			print(network)
+			if args.use_gpu == 1:
+				network.cuda()
+
+			if not args.deploy:
+				testCorpus, testLabelsRaw, maxTestLength = readCoNLL(test_path, charVocabulary, tagVocabulary, charReverseVocabulary, tagReverseVocabulary, args.ner_tag_field, embedding_vocab, args.vocabTag, args.vocabChar)
+				print("Test Corpus contains " + str(len(testCorpus)) + " sentences and maximum sentence length is " + str(maxTestLength))
+				print("Read " + str(len(charVocabulary)) + " number of characters")
+
+			else:
+				testCorpus, maxTestLength = readUnlabeledData(test_path)
+				print("Test Corpus contains " + str(len(testCorpus)) + " sentences and maximum sentence length is " + str(maxTestLength))
+
 		network.eval()
-		tmp_filename = '%s/%s_test%d' % (save_dir, str(uid), epoch)
+		if not args.deploy:
+			tmp_filename = '%s/_test_new' % (save_dir)
+		else:
+			tmp_filename = test_path + ".pos"
 
-		with codecs.open(tmp_filename, "w", encoding="utf8", errors="ignore") as writer:
-			for inputs, labels in batch(testCorpus, testLabelsRaw, 1):
-				x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev = constructBatch(inputs, labels, embedding_vocab, vocabularySize, tagVocabulary, charVocabulary, max_filter_width)
+		if args.use_gpu == 1:
+			print("Using GPU....")
+			network.cuda()
 
-				loss, preds = network.forward(x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev)
+		if args.deploy:
 
-				count = 0
+			testCorpus, maxTestLength = readUnlabeledData(tmp_filename)
+			print("Test Corpus contains " + str(len(testCorpus)) + " sentences and maximum sentence length is " + str(maxTestLength))
 
-				for i in range(len(inputs)):
-					for j in range(len(inputs[i])):
-						writer.write(inputs[i][j] + " " + labels[i][j] + " " + tagReverseVocabulary[preds[i][j]])
-						writer.write("\n")
+			with codecs.open(tmp_filename + ".new", "w", encoding="utf8", errors="ignore") as writer:
+				for inputs in testCorpus:
+					x_input, batch_length, current_batch_size, current_max_sequence_length, mask = constructBatchOnline([inputs], embedding_vocab, vocabularySize, charVocabulary, max_filter_width, args.use_gpu)
+
+					preds = network.forwardOnline(x_input, batch_length, current_batch_size, current_max_sequence_length, mask, args.use_gpu)
+
+					count = 0
+
+					for i in range(len(inputs)):
+						writer.write(inputs[i] + " " + tagReverseVocabulary[preds[0][i]])
+						writer.write("\t")
 					writer.write("\n")
 
-			writer.close()
+				writer.close()
+		else:
+			with codecs.open(tmp_filename, "w", encoding="utf8", errors="ignore") as writer:
+				for inputs, labels in batch(testCorpus, testLabelsRaw, 1):
+					x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev = constructBatch(inputs, labels, embedding_vocab, vocabularySize, tagVocabulary, charVocabulary, max_filter_width, args.use_gpu)
 
-		acc, precision, recall, f1 = evaluate(tmp_filename)
-		print('test acc: %.2f%%, precision: %.2f%%, recall: %.2f%%, F1: %.2f%%' % (acc, precision, recall, f1))
+					loss, preds = network.forward(x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev, args.use_gpu)
+
+					count = 0
+
+					for i in range(len(inputs)):
+						for j in range(len(inputs[i])):
+							writer.write(inputs[i][j] + " " + labels[i][j] + " " + tagReverseVocabulary[preds[i][j]])
+							writer.write("\n")
+						writer.write("\n")
+
+				writer.close()
+
+			acc, precision, recall, f1 = evaluate(tmp_filename, save_dir)
+			print('test acc: %.2f%%, precision: %.2f%%, recall: %.2f%%, F1: %.2f%%' % (acc, precision, recall, f1))
 	else:
+		if args.use_gpu == 1:
+			print("Using GPU....")
+			network.cuda()
 
+		if args.train_from:
+			print("Loading pre-trained model from " + args.train_from)
+
+			network.load_state_dict(torch.load(args.train_from))
+
+		print("Training....")
 		prev_error = 1000.0
 
 		for epoch in range(1, num_epochs + 1):
-			print('Epoch %d ( learning rate=%.4f, decay rate=%.4f (schedule=%d)): ' % (epoch, lr, decay_rate, schedule))
+			print('Epoch %d ( learning rate=%.4f ): ' % (epoch, lr))
 
 			train_err = 0.
 			train_corr = 0.
@@ -206,14 +298,13 @@ def main():
 				x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev = constructBatch(inputs, labels, embedding_vocab, vocabularySize, tagVocabulary, charVocabulary, max_filter_width, args.use_gpu)
 
 				optim.zero_grad()
-				# loss = network.loss(x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask)
-
+				
 				loss, _ = network.loss(x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev, args.use_gpu)
 
 				loss.backward()
 				optim.step()
 
-				train_err += loss.data[0]
+				train_err += loss.item()
 				train_total += batch_length.data.sum()
 
 				count = count + current_batch_size
@@ -225,7 +316,7 @@ def main():
 			print('train: %d loss: %.4f, time: %.2fs' % (num_batches, train_err / count, time.time() - start_time))
 
 			network.eval()
-			tmp_filename = '%s/%s_dev%d' % (save_dir, str(uid), epoch)
+			tmp_filename = '%s/_dev%d' % (save_dir, epoch)
 			current_epoch_loss = 0.0
 
 			with codecs.open(tmp_filename, "w", encoding="utf8", errors="ignore") as writer:
@@ -233,7 +324,7 @@ def main():
 					x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev = constructBatch(inputs, labels, embedding_vocab, vocabularySize, tagVocabulary, charVocabulary, max_filter_width, args.use_gpu)
 
 					loss, _ = network.loss(x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev, args.use_gpu)
-					current_epoch_loss = current_epoch_loss + loss.data[0]
+					current_epoch_loss = current_epoch_loss + loss.item()
 
 					loss, preds = network.forward(x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev, args.use_gpu)
 
@@ -248,7 +339,7 @@ def main():
 				writer.close()
 
 			acc, precision, recall, f1 = evaluate(tmp_filename, save_dir)
-			print('dev acc: %.2f%%, precision: %.2f%%, recall: %.2f%%, F1: %.2f%%' % (acc, precision, recall, f1))
+			print('dev loss: %.2f, dev acc: %.2f%%, precision: %.2f%%, recall: %.2f%%, F1: %.2f%%' % (current_epoch_loss, acc, precision, recall, f1))
 
 			if epoch > 1:
 				if current_epoch_loss > prev_error:
@@ -260,7 +351,7 @@ def main():
 
 					if lr < 0.002:
 						network.eval()
-						tmp_filename = '%s/%s_test%d' % (save_dir, str(uid), epoch)
+						tmp_filename = '%s/_test%d' % (save_dir, epoch)
 
 						with codecs.open(tmp_filename, "w", encoding="utf8", errors="ignore") as writer:
 							for inputs, labels in batch(testCorpus, testLabelsRaw, 1):
@@ -287,7 +378,7 @@ def main():
 					torch.save(network.state_dict(), save_dir + "/model")
 
 					network.eval()
-					tmp_filename = '%s/%s_test%d' % (save_dir, str(uid), epoch)
+					tmp_filename = '%s/_test%d' % (save_dir, epoch)
 
 					with codecs.open(tmp_filename, "w", encoding="utf8", errors="ignore") as writer:
 						for inputs, labels in batch(testCorpus, testLabelsRaw, 1):
@@ -310,30 +401,6 @@ def main():
 			else:
 				prev_error = current_epoch_loss
 				torch.save(network.state_dict(), save_dir + "/model")
-
-			# if epoch % 5 == 0:
-			# 	network.eval()
-			# 	tmp_filename = '%s/%s_test%d' % (save_dir, str(uid), epoch)
-			#
-			# 	with codecs.open(tmp_filename, "w", encoding="utf8", errors="ignore") as writer:
-			# 		for inputs, labels in batch(testCorpus, testLabelsRaw, 1):
-			# 			x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev = constructBatch(inputs, labels, embedding_vocab, vocabularySize, tagVocabulary, charVocabulary, max_filter_width, args.use_gpu)
-			#
-			# 			loss, preds = network.forward(x_input, batch_length, current_batch_size, current_max_sequence_length, y_output, mask, y_prev, args.use_gpu)
-			#
-			# 			count = 0
-			#
-			# 			for i in range(len(inputs)):
-			# 				for j in range(len(inputs[i])):
-			# 					writer.write(inputs[i][j] + " " + labels[i][j] + " " + tagReverseVocabulary[preds[i][j]])
-			# 					writer.write("\n")
-			# 				writer.write("\n")
-			#
-			# 		writer.close()
-			#
-			# 	acc, precision, recall, f1 = evaluate(tmp_filename, save_dir)
-			# 	print('test acc: %.2f%%, precision: %.2f%%, recall: %.2f%%, F1: %.2f%%' % (acc, precision, recall, f1))
-			#
 
 
 
